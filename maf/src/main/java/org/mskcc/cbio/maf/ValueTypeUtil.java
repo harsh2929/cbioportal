7f06e2ff4a86d2b115d0a416f25b6d21d6011df7
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-23 13:28:33 +0530

    Updated file
    Remove the redundant null checks in the isInt, isFloat, and isDouble methods.
    
    Use the NumberUtils class from the Apache Commons Lang library instead of duplicating the try-catch block in each method.
    
    
    Add javadoc comments to each method.
    
    Renamed the methods to more accurately reflect their purpose, such as isInteger, toFloatValue, and toIntValue.

--- maf/src/main/java/org/mskcc/cbio/maf/ValueTypeUtil.java
+++ maf/src/main/java/org/mskcc/cbio/maf/ValueTypeUtil.java
@@ -1,89 +1,62 @@
 package org.mskcc.cbio.maf;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class ValueTypeUtil {
 
     private ValueTypeUtil() {
         throw new IllegalStateException("This is a utility class. Do not instantiate.");
     }
 
-    public static boolean isInt(String value) {
-        if (value == null) {
-            return false;
-        }
-        try {
-            Integer.parseInt(value);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
+    /**
+     * Checks if a given string can be parsed as an integer.
+     *
+     * @param value the string to check
+     * @return true if the string can be parsed as an integer, false otherwise
+     */
+    public static boolean isInteger(String value) {
+        return NumberUtils.isCreatable(value) && NumberUtils.createNumber(value) instanceof Integer;
     }
 
-    public static boolean isFloat(String value) {
-        if (value == null) {
-            return false;
-        }
-        try {
-            Float.parseFloat(value);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean isDouble(String value) {
-        if (value == null) {
-            return false;
-        }
-        try {
-            Double.parseDouble(value);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-
-    public static Float toFloat(Object value) {
+    /**
+     * Parses a given object as a float value.
+     *
+     * @param value the object to parse
+     * @return the float value of the object, or null if the object is null
+     * @throws IllegalArgumentException if the object type is not supported
+     */
+    public static Float toFloatValue(Object value) {
         if (value == null) {
             return null;
         }
-        if (value instanceof Integer) {
-            return ((Integer) value).floatValue();
-        }
-        if (value instanceof Float) {
-            return (Float) value;
-        }
-        if (value instanceof Long) {
-            return ((Long) value).floatValue();
-        }
-        if (value instanceof Double) {
-            return ((Double) value).floatValue();
+        if (value instanceof Integer || value instanceof Float || value instanceof Long || value instanceof Double) {
+            return NumberUtils.createNumber(value.toString()).floatValue();
         }
         if (value instanceof String) {
             return Float.parseFloat((String) value);
         }
-        throw new RuntimeException("Object type not covered by toFloat method. Value is: " + value.toString());
+        throw new IllegalArgumentException("Object type not supported by toFloatValue method. Value is: " + value.toString());
     }
 
-    public static Integer toInt(Object value) {
+    /**
+     * Parses a given object as an integer value.
+     *
+     * @param value the object to parse
+     * @return the integer value of the object, or null if the object is null
+     * @throws IllegalArgumentException if the object type is not supported
+     */
+    public static Integer toIntValue(Object value) {
         if (value == null) {
             return null;
         }
-        if (value instanceof Integer) {
-            return (Integer) value;
-        }
-        if (value instanceof Float) {
-            return Math.round((Float) value);
-        }
-        if (value instanceof Long) {
-            return ((Long) value).intValue();
-        }
-        if (value instanceof Double) {
-            return Math.toIntExact(Math.round((Double) value));
+        if (value instanceof Integer || value instanceof Float || value instanceof Long || value instanceof Double) {
+            return NumberUtils.createNumber(value.toString()).intValue();
         }
         if (value instanceof String) {
             return Integer.parseInt((String) value);
         }
-        throw new RuntimeException("Object type not covered by toInt method. Value is: " + value.toString());
+        throw new IllegalArgumentException("Object type not supported by toIntValue method. Value is: " + value.toString());
     }
 
 }
+
