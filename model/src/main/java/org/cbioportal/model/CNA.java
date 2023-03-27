b000ddad6920eaeb420509b52bd70eb587a3301a
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-27 21:56:54 +0530

    Updated CNA. java
    Use diamond operator.
    Make the cache final.
    Added null safety.

--- model/src/main/java/org/cbioportal/model/CNA.java
+++ model/src/main/java/org/cbioportal/model/CNA.java
@@ -4,35 +4,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum CNA {
-    AMP ((short)2, "Amplified"),
-    GAIN ((short)1, "Gained"),
-    DIPLOID ((short)0, "Diploid"),
-    HETLOSS ((short)-1, "Heterozygously deleted"),
-    HOMDEL ((short)-2, "Homozygously deleted");
-    
+    AMP((short)2, "Amplified"),
+    GAIN((short)1, "Gained"),
+    DIPLOID((short)0, "Diploid"),
+    HETLOSS((short)-1, "Heterozygously deleted"),
+    HOMDEL((short)-2, "Homozygously deleted");
+
     private short code;
     private String desc;
-    
+
     private CNA(short code, String desc) {
         this.code = code;
         this.desc = desc;
     }
-    
-    private final static Map<Short, CNA> cache = new HashMap<Short, CNA>();
+
+    private static final Map<Short, CNA> cache = new HashMap<>();
+
     static {
         for (CNA cna : CNA.values()) {
             cache.put(cna.code, cna);
         }
     }
-    
+
     public static CNA getByCode(short code) {
-        return cache.get(code);
+        CNA cna = cache.get(code);
+        if (cna == null) {
+            throw new IllegalArgumentException("Invalid CNA code: " + code);
+            // or you could return a default value instead of throwing an exception, e.g.
+            // return DIPLOID;
+        }
+        return cna;
     }
-    
+
     public short getCode() {
         return code;
     }
-    
+
     public String getDescription() {
         return desc;
     }
