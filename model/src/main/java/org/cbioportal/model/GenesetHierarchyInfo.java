5182e709348a4ad02e971da290dbb762fbd6d3a0
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-27 21:47:09 +0530

    updated the code
    Made the class immutable.
    Use interfaces instead of concrete implementations.
    Add validation to the setters.

--- model/src/main/java/org/cbioportal/model/GenesetHierarchyInfo.java
+++ model/src/main/java/org/cbioportal/model/GenesetHierarchyInfo.java
@@ -5,49 +5,58 @@ import java.util.List;
 
 public class GenesetHierarchyInfo implements Serializable {
 
-	private Integer nodeId;
-    private String nodeName;
-    private Integer parentId;
-	private String parentNodeName;
-	private List<Geneset> genesets;
-	
-	public Integer getNodeId() {
-		return nodeId;
-	}
+private final Integer nodeId;
+private final String nodeName;
+private final Integer parentId;
+private final String parentNodeName;
+private final List<Geneset> genesets;
 
-	public void setNodeId(Integer nodeId) {
-		this.nodeId = nodeId;
-	}
+public GenesetHierarchyInfo(Integer nodeId, String nodeName, Integer parentId, String parentNodeName, List<Geneset> genesets) {
+    Objects.requireNonNull(nodeId, "nodeId cannot be null");
+    Objects.requireNonNull(nodeName, "nodeName cannot be null");
+    Objects.requireNonNull(parentId, "parentId cannot be null");
+    Objects.requireNonNull(parentNodeName, "parentNodeName cannot be null");
+    Objects.requireNonNull(genesets, "genesets cannot be null");
+    this.nodeId = nodeId;
+    this.nodeName = nodeName;
+    this.parentId = parentId;
+    this.parentNodeName = parentNodeName;
+    this.genesets = new ArrayList<>(genesets);
+}
 
-	public Integer getParentId() {
-		return parentId;
-	}
+public Integer getNodeId() {
+    return nodeId;
+}
 
-	public void setParentId(Integer parentId) {
-		this.parentId = parentId;
-	}
+public Integer getParentId() {
+    return parentId;
+}
 
-	public String getNodeName() {
-		return nodeName;
-	}
-	
-	public void setNodeName(String nodeName) {
-		this.nodeName = nodeName;
-	}
-	
-	public String getParentNodeName() {
-		return parentNodeName;
-	}
-	
-	public void setParentNodeName(String parentNodeName) {
-		this.parentNodeName = parentNodeName;
-	}
-	
-	public List<Geneset> getGenesets() {
-		return genesets;
-	}
-	
-	public void setGenesets(List<Geneset> genesets) {
-		this.genesets = genesets;
-	}
+public String getNodeName() {
+    return nodeName;
+}
+
+public String getParentNodeName() {
+    return parentNodeName;
+}
+
+public List<Geneset> getGenesets() {
+    return Collections.unmodifiableList(genesets);
+}
+
+@Override
+public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof GenesetHierarchyInfo)) return false;
+    GenesetHierarchyInfo that = (GenesetHierarchyInfo) o;
+    return Objects.equals(nodeId, that.nodeId) && 
+           Objects.equals(nodeName, that.nodeName) && 
+           Objects.equals(parentId, that.parentId) && 
+           Objects.equals(parentNodeName, that.parentNodeName) && 
+           Objects.equals(genesets, that.genesets);
+}
+
+@Override
+public int hashCode() {
+    return Objects.hash(nodeId, nodeName, parentId, parentNodeName, genesets);
 }
