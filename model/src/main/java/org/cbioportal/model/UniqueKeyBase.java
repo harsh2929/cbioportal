ceb2a83bebae43243a45fd666ae4b1419dc86dcd
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-27 21:41:12 +0530

    updated the code
    Added validation to the setters.
    Make the class immutable.
    Override the equals() and hashCode() methods.

--- model/src/main/java/org/cbioportal/model/UniqueKeyBase.java
+++ model/src/main/java/org/cbioportal/model/UniqueKeyBase.java
@@ -1,25 +1,34 @@
 package org.cbioportal.model;
 
 import java.io.Serializable;
+private final String uniqueSampleKey;
+private final String uniquePatientKey;
 
-public abstract class UniqueKeyBase implements Serializable {
-    
-    private String uniqueSampleKey;
-    private String uniquePatientKey;
+public UniqueKeyBase(String uniqueSampleKey, String uniquePatientKey) {
+    Objects.requireNonNull(uniqueSampleKey, "uniqueSampleKey cannot be null");
+    Objects.requireNonNull(uniquePatientKey, "uniquePatientKey cannot be null");
+    this.uniqueSampleKey = uniqueSampleKey;
+    this.uniquePatientKey = uniquePatientKey;
+}
 
-    public String getUniqueSampleKey() {
-        return uniqueSampleKey;
-    }
+public String getUniqueSampleKey() {
+    return uniqueSampleKey;
+}
 
-    public void setUniqueSampleKey(String uniqueSampleKey) {
-        this.uniqueSampleKey = uniqueSampleKey;
-    }
+public String getUniquePatientKey() {
+    return uniquePatientKey;
+}
 
-    public String getUniquePatientKey() {
-        return uniquePatientKey;
-    }
+@Override
+public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof UniqueKeyBase)) return false;
+    UniqueKeyBase that = (UniqueKeyBase) o;
+    return Objects.equals(uniqueSampleKey, that.uniqueSampleKey) && 
+           Objects.equals(uniquePatientKey, that.uniquePatientKey);
+}
 
-    public void setUniquePatientKey(String uniquePatientKey) {
-        this.uniquePatientKey = uniquePatientKey;
-    }
+@Override
+public int hashCode() {
+    return Objects.hash(uniqueSampleKey, uniquePatientKey);
 }
