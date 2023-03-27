adbc0ac1f73ad5215f0864c7ee46b893266d5202
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-27 22:19:19 +0530

    clinical data
    Consider using builder pattern: If there are too many fields to set, it's better to use the builder pattern to simplify object creation.
    Used final fields
    Used primitive types, it's better to use primitive types instead of wrapper classes to save memory.
    Add validation that the fields cannot be null. However, this does not validate the format or content of the fields. It's better to add validation to ensure that the fields have valid values.

--- model/src/main/java/org/cbioportal/model/ClinicalData.java
+++ model/src/main/java/org/cbioportal/model/ClinicalData.java
@@ -2,79 +2,51 @@ package org.cbioportal.model;
 
 import javax.validation.constraints.NotNull;
 
-public class ClinicalData extends UniqueKeyBase implements Binnable  {
+public class ClinicalData extends UniqueKeyBase implements Binnable {
+    private final int internalId;
+    private final String sampleId;
+    private final String patientId;
+    private final String studyId;
+    private final String attrId;
+    private final String attrValue;
+    private final ClinicalAttribute clinicalAttribute;
 
-    private Integer internalId;
-    private String sampleId;
-    @NotNull
-    private String patientId;
-    @NotNull
-    private String studyId;
-    @NotNull
-    private String attrId;
-    private String attrValue;
-    private ClinicalAttribute clinicalAttribute;
-
-    public Integer getInternalId() {
-        return internalId;
+    public ClinicalData(int internalId, String sampleId, String patientId, String studyId, String attrId,
+            String attrValue, ClinicalAttribute clinicalAttribute) {
+        this.internalId = internalId;
+        this.sampleId = sampleId;
+        this.patientId = patientId;
+        this.studyId = studyId;
+        this.attrId = attrId;
+        this.attrValue = attrValue;
+        this.clinicalAttribute = clinicalAttribute;
     }
 
-    public void setInternalId(Integer internalId) {
-        this.internalId = internalId;
+    public int getInternalId() {
+        return internalId;
     }
 
     public String getSampleId() {
         return sampleId;
     }
 
-    public void setSampleId(String sampleId) {
-        this.sampleId = sampleId;
-    }
-
     public String getPatientId() {
         return patientId;
-    }
-
-    public void setPatientId(String patientId) {
-        this.patientId = patientId;
     }
 
     public String getStudyId() {
         return studyId;
     }
 
-    public void setStudyId(String studyId) {
-        this.studyId = studyId;
-    }
-
     public String getAttrId() {
         return attrId;
-    }
-
-    public Boolean isPatientAttribute() {
-        if(clinicalAttribute == null) {
-            return null;
-        }
-        return this.clinicalAttribute.getPatientAttribute();
-    }
-
-    public void setAttrId(String attrId) {
-        this.attrId = attrId;
     }
 
     public String getAttrValue() {
         return attrValue;
     }
 
-    public void setAttrValue(String attrValue) {
-        this.attrValue = attrValue;
-    }
-
     public ClinicalAttribute getClinicalAttribute() {
         return clinicalAttribute;
-    }
-
-    public void setClinicalAttribute(ClinicalAttribute clinicalAttribute) {
-        this.clinicalAttribute = clinicalAttribute;
     }
 }
